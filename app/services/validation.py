ed1dedb520642aeda194eb1fa733fa7f60ed1c8f
artem <zhiruyev_a@bankrbk.kz>
2023-11-19 11:10:35 +0600

    classmethod changed to staticmethod

--- app/services/validation.py
+++ app/services/validation.py
@@ -10,8 +10,8 @@ from app.routers.entities.input.get_form_input_entity import (
 
 
 class GetFormQueryParamFactory:
-    @classmethod
-    def get_form_query_param(cls, field_name: str, field_value: str):
+    @staticmethod
+    def get_form_query_param(field_name: str, field_value: str):
         possible_classes = [
             GetFormDateQueryParam,
             GetFormPhoneQueryParam,
